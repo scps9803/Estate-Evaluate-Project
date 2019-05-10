d6da68e061f9e346c4395a29e730d322d2607d4a
scps9803 <scps9803@gmail.com>
2019-05-10 17:29:00 +0800

    Version 0.0.2

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// <input type="radio" name="compensate-extra-1" value="">一般<input type="radio" name="compensate-extra-1" value="">閣樓板</td>
+$(document).ready(function(){
+
+    $("#compensate-form-1").click(function(){
+        window.alert("123");
+    });
+
+});
